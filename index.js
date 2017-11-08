14b1b276e5e2833aa59b8e29f09682162f81081e
Mankirat Singh <mankirat1998@gmail.com>
2017-11-09 02:20:29 +0530

    fix: bug fixes

--- index.js
+++ index.js
@@ -19,41 +19,41 @@ app.get('/', function (req, res) {
 });
 
 //Facebook
-app.get('/webhook/', function (req, res) {
-    if(req.query['hub.verify_token'] === 'password'){
-        res.send(req.query['hub.challenge']);
+app.get('/webhook/', function(req, res) {
+    if (req.query['hub.verify_token'] === "password") {
+        return res.send(req.query['hub.challenge']);
+    }else{
+        res.send("Wrong token");
     }
-    res.send("Wrong  Token");
 });
 
-app.post('/webhook/', function (req, res) {
-    var messaging_events= req.body.entry[0].messaging;
-    for(var i=0;i<messaging_events.length;i++){
-        var event= messaging_events[i];
-        var sender= event.sender.id;
-        if(event.message && event.message.text){
-            var text= event.message.text;
-            sendText(sender,"Text echo:" + text.substring(0,100));
+app.post('/webhook/', function(req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = messaging_events[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendText(sender, "Text echo: " + text.substring(0, 100));
         }
     }
-
     res.sendStatus(200);
 });
 
 function sendText(sender, text) {
-    var messageData={text:text};
+    let messageData = {text: text}
     request({
-        url:"https://graph.facebook.com/v2.6/me/messages",
-        qs:{access_token: token},
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs : {access_token: token},
         method: "POST",
-        json:{
-            recipient:{id: sender},
-            message:messageData
+        json: {
+            recipient: {id: sender},
+            message : messageData,
         }
-    }, function (error, response, body) {
-        if(error){
+    }, function(error, response, body) {
+        if (error) {
             console.log("sending error");
-        }else if(response.body.error){
+        } else if (response.body.error) {
             console.log("response body error");
         }
     });
