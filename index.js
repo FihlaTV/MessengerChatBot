fe3766b058f9ff12e720eee883325d7e52ad67be
Mankirat Singh <mankirat1998@gmail.com>
2017-11-09 21:04:43 +0530

    feat: basic ML added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict";
 
-var express= require("express");
-var bodyParser= require("body-parser");
-var request= require("request");
-var secret= require("./secret.json");
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require("request");
+var secret = require("./secret.json");
 
-var app= express();
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 var token = secret.app_token;
-var password= secret.app_secret;
+var password = secret.app_secret;
 
 //Routes
 app.get('/', function (req, res) {
@@ -21,38 +21,76 @@ app.get('/', function (req, res) {
 });
 
 //Facebook
-app.get('/webhook/', function(req, res) {
+app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === password) {
         return res.send(req.query['hub.challenge']);
-    }else{
+    } else {
         res.send("Wrong token");
     }
 });
 
-app.post('/webhook/', function(req, res) {
+app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
         let event = messaging_events[i]
         let sender = event.sender.id
         if (event.message && event.message.text) {
-            let text = event.message.text
-            sendText(sender, "Text echo: " + text.substring(0, 100));
+            let text = event.message.text;
+            decideMessage(sender, text);
+            // sendText(sender, "Text echo: " + text.substring(0, 100));
         }
     }
     res.sendStatus(200);
 });
 
+function decideMessage(sender, text1) {
+    let text = text1.toLowerCase();
+    if (text.includes('download')) {
+        sendButtonMessage(sender);
+    } else if (text.includes('problem') || text.includes('bad')) {
+        sendText(sender,"Hi i am a bot, We are sorry for your bad expirience. A member of our team will respond soon.");
+    } else {
+        sendText(sender,"Hi i am a bot, A member from our team will get back to you shortly");
+    }
+}
+
+function sendButtonMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "button",
+                    "text": "Download our app from here",
+                "buttons": [
+                    {
+                        "type": "web_url",
+                        "url": "https://play.google.com/store/apps/details?id=com.zcorp.game.othello&hl=en",
+                        "title": "Othello",
+                        "webview_height_ratio": "full",
+                        "messenger_extensions": true,
+                    },
+                ]
+            }
+        }
+    };
+    sendRequest(sender,messageData);
+}
+
 function sendText(sender, text) {
-    let messageData = {text: text}
+    let messageData = {text: text};
+    sendRequest(sender, messageData);
+}
+
+function sendRequest(sender, messageData) {
     request({
         url: "https://graph.facebook.com/v2.6/me/messages",
-        qs : {access_token: token},
+        qs: {access_token: token},
         method: "POST",
         json: {
             recipient: {id: sender},
-            message : messageData,
+            message: messageData,
         }
-    }, function(error, response, body) {
+    }, function (error, response, body) {
         if (error) {
             console.log("sending error");
         } else if (response.body.error) {
